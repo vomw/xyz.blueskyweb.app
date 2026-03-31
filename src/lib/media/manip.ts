0ed816aa1441f2e47b8b29b9b74bf9dd39605ebc
Samuel Newman <mozzius@protonmail.com>
2026-03-31 15:30:54 +0300

    remove buffer import

--- src/lib/media/manip.ts
+++ src/lib/media/manip.ts
@@ -15,7 +15,6 @@ import {
 import {manipulateAsync, SaveFormat} from 'expo-image-manipulator'
 import * as MediaLibrary from 'expo-media-library'
 import * as Sharing from 'expo-sharing'
-import {Buffer} from 'buffer'
 
 import {POST_IMG_MAX} from '#/lib/constants'
 import {logger} from '#/logger'
